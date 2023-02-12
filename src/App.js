a43d037273e19719aa2f2fac00bf5cc3a9867742
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-02-12 14:16:50 +0700

    Downgrade reactt-version

--- src/App.js
+++ src/App.js
@@ -1,110 +1,94 @@
-import React,{useEffect,lazy,Suspense} from 'react';
-import Header from './component/header/header.component'
-import { Switch, Route} from 'react-router-dom'
-import styled from 'styled-components'
-import NavBar from './component/navbar/navbar.component'
-import {fetchCollectionsStart} from './redux/store/store.action'
-import {connect} from 'react-redux'
-import Spinner from './component/spinner/spinner.component';
-import ErrorBoundary from './component/error-boundary/Error-Boundary.component';
-import {checkUserSession} from './redux/user/user.action'
-import { firestore } from './firebase/firebase.utils';
-import firebase from 'firebase/compat/app';
+import React, { useEffect, lazy, Suspense } from "react";
+import Header from "./component/header/header.component";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import NavBar from "./component/navbar/navbar.component";
+import { fetchCollectionsStart } from "./redux/store/store.action";
+import { connect } from "react-redux";
+import Spinner from "./component/spinner/spinner.component";
+import ErrorBoundary from "./component/error-boundary/Error-Boundary.component";
+import { checkUserSession } from "./redux/user/user.action";
+import { firestore } from "./firebase/firebase.utils";
 
-const Application = lazy(()=>import('./page/Application/Application.page'))
-const EntertainmentPage = lazy(()=>import('./page/Entertainment/Entertainment.page'))
-const Movie = lazy(()=>import('./page/Movie/Movie.page'))
-const Book = lazy(()=>import('./page/Book/Book.page'))
-const CheckoutPage = lazy(()=>import('./page/Checkout/CheckoutPage.page'))
+const Application = lazy(() => import("./page/Application/Application.page"));
+const EntertainmentPage = lazy(() =>
+  import("./page/Entertainment/Entertainment.page")
+);
+const Movie = lazy(() => import("./page/Movie/Movie.page"));
+const Book = lazy(() => import("./page/Book/Book.page"));
+const CheckoutPage = lazy(() => import("./page/Checkout/CheckoutPage.page"));
 
+const App = ({ fetchCollectionsStart, checkUserSession }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+    checkUserSession();
+  }, [fetchCollectionsStart]);
+  const date = new Date();
 
-
-const App = ({fetchCollectionsStart,checkUserSession})=>{
-
-    useEffect(() => {
-      fetchCollectionsStart()
-      checkUserSession()
-    }, [fetchCollectionsStart])
-    const date = new Date()
-
-    useEffect(()=>{
-      let Ref = firestore.collection('store').doc('movie');
-      // obj.map(item=>{
-      //   // Ref.update({
-      //   //   categories: firebase.firestore.FieldValue.arrayUnion(item)
-      //   // })
-      // })
-      // Ref.update({
-      //   categories: firebase.firestore.FieldValue.arrayUnion(obj)
-      // });
-
-    },[])
-
-    return(
-      <Container>
-         <Header/>
-         <AppWrapper>
-          <NavWrapper>
-              <NavBar/>
-          </NavWrapper>
-          <SwitchWrapper>
+  return (
+    <Container>
+      <Header />
+      <AppWrapper>
+        <NavWrapper>
+          <NavBar />
+        </NavWrapper>
+        <SwitchWrapper>
           <ErrorBoundary>
             <Switch>
-              <Suspense fallback={<Spinner/>}>
-                <Route  path='/entertainment' component={EntertainmentPage} />
-                <Route path='/movies' component={Movie} />
-                <Route path='/checkout' component={CheckoutPage}/>
-                <Route  path='/books' component={Book} />
+              <Suspense fallback={<Spinner />}>
+                <Route path="/entertainment" component={EntertainmentPage} />
+                <Route path="/movies" component={Movie} />
+                <Route path="/checkout" component={CheckoutPage} />
+                <Route path="/books" component={Book} />
               </Suspense>
             </Switch>
-            </ErrorBoundary>
-          </SwitchWrapper> 
-         </AppWrapper> 
-      </Container>
-    )
-}
+          </ErrorBoundary>
+        </SwitchWrapper>
+      </AppWrapper>
+    </Container>
+  );
+};
 
 const NavWrapper = styled.div`
-  z-index:1;
+  z-index: 1;
 
-  @media screen and (max-width:800px){
+  @media screen and (max-width: 800px) {
     display: block;
-    width:100%;
+    width: 100%;
   }
-`
+`;
 
 const SwitchWrapper = styled.div`
   height: calc(100vh - 60px);
   width: calc(100vw - 14vw);
   overflow-y: auto;
-   
-  &::-webkit-scrollbar{
-    display: none;
-  } 
 
-  
-  @media screen and (max-width:800px){
-    display: block;
-    width:100%;
+  &::-webkit-scrollbar {
+    display: none;
   }
-`
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    width: 100%;
+  }
+`;
 
 const AppWrapper = styled.div`
   display: flex;
 
-  @media screen and (max-width:800px){
-    display:block;
+  @media screen and (max-width: 800px) {
+    display: block;
   }
-`
+`;
 
 const Container = styled.div`
-   background-color: #f3f3f4!important;
-   bottom:0px;
-`
+  background-color: #f3f3f4 !important;
+  bottom: 0px;
+`;
 
-const mapDispatchToProps = dispatch=>({
-    checkUserSession:()=>dispatch(checkUserSession()),
-    fetchCollectionsStart:()=>dispatch(fetchCollectionsStart())
-})
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
--- src/component/navbar/Entertainment.jsx
+++ src/component/navbar/Entertainment.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import {AccWrapper,AccSum} from './navbar.component'
+import React from "react";
+import { AccWrapper, AccSum } from "./navbar.component";
 import { BsGridFill } from "react-icons/bs";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import {selectNavBar} from '../../redux/navbar/navbar.selector'
-import {toggleNavBar} from '../../redux/navbar/navbar.action'
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectNavBar } from "../../redux/navbar/navbar.selector";
+import { toggleNavBar } from "../../redux/navbar/navbar.action";
 
-const Entertainment= ({history,match,panel,toggleNavBar})=>{
-    return (
-        <AccWrapper 
-        square="true" 
-        className="Entertainment"
-        expanded={panel === 'panel1'} 
-        onChange={()=>toggleNavBar('panel1')}
-        onClick = {()=>history.push(`${match.url}entertainment`)}
-        >
-        <AccSum   aria-controls="panel1d-content" id="panel1d-header">
-          <BsGridFill/>
-          <span>Entertainment</span>
-        </AccSum>
-      </AccWrapper>
-    )
-}
-
+const Entertainment = ({ history, match, panel, toggleNavBar }) => {
+  return (
+    <AccWrapper
+      square="true"
+      className="Entertainment"
+      expanded={panel === "panel1"}
+      onChange={() => toggleNavBar("panel1")}
+      onClick={() => history.push(`${match.url}entertainment`)}
+    >
+      <AccSum aria-controls="panel1d-content" id="panel1d-header">
+        <BsGridFill />
+        <span>Entertainment</span>
+      </AccSum>
+    </AccWrapper>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  panel:selectNavBar
-})
-
-const mapDispatchToProps = dispatch => ({
-  toggleNavBar:panel => dispatch(toggleNavBar(panel)),
+  panel: selectNavBar,
 });
 
-export default React.memo(connect(mapStateToProps,mapDispatchToProps)(Entertainment));
+const mapDispatchToProps = (dispatch) => ({
+  toggleNavBar: (panel) => dispatch(toggleNavBar(panel)),
+});
+
+export default React.memo(
+  connect(mapStateToProps, mapDispatchToProps)(Entertainment)
+);
